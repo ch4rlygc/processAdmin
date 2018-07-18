cefc3ce7136e4f22537859908ac824343a1ae5eb
carlos <carlos.jgc14@gmail.com>
2018-07-18 01:26:39 -0600

    changes
    changes in some parts, redirect isn't working.

--- js/login.js
+++ js/login.js
@@ -4,17 +4,21 @@ $( document ).ready(function() {
 
 		function userValidation () {
 
-			if ( $("loginUser").val().length > 0 ) {
+			if ( $("#loginUser").val().length > 0 ) {
 				var strUserName = localStorage.getItem("NewUserName");
 				var strPassword = localStorage.getItem("NewPassword");
 
-				if ( $("loginUser").value == strUserName && $("loginPass".value == strPassword) ) {
+				if ( $("input#loginUser").val() === strUserName && $("input#loginPass").val() === strPassword ) {
 
 					$("#successMessage").removeClass("d-none");
 					$("#successMessage").append("Inicio de sesión exitoso!");
 					alert("Inicio de sesión exitoso");
 
-					location.href="dashboard.html";
+					function redirect () {
+						location.href = 'https://www.youtube.com/';
+					}
+					redirect();
+
 				}
 				else {
 					function showError () {
--- js/register.js
+++ js/register.js
@@ -1,43 +1,56 @@
 $(document).ready(function(){
 
 	$("#registerButton").click(function(){
+
+	    localStorage.clear();
 		
-		var validPass = $("#formPass").value;
-		var validPass1 = $("#formPass1").value;
+		var validName = $("input#formName").val().length;
+		var validUser = $("input#formUser").val();
+		var validUserType = $("input#formUserType").val();
+		var validPass = $("input#formPass").val();
+		var validPass1 = $("input#formPass1").val();
 
-		if (validPass == validPass1) {
+		if (validName > 0) {
+            if (validPass === validPass1) {
 
-			var strName = $("#formName").value;
-			var strUser = $("#formUser").value;
-			var strUserType = $("#formUserType").value;
-			var strPass = $("#formPass").value;
+                var strName = $("input#formName").val();
+                var strUser = $("input#formUser").val();
+                var strUserType = $("input#formUserType").val();
+                var strPass = $("input#formPass").val();
 
-			function storeData () {
-				localStorage.setItem('NewNombre', strName);
-				localStorage.setItem('NewUserName', strUser);
-				localStorage.setItem('NewUserType', strUserType);
-				localStorage.setItem('NewPassword', strPass);
+                function storeData () {
+                    localStorage.setItem('NewNombre', strName);
+                    localStorage.setItem('NewUserName', strUser);
+                    localStorage.setItem('NewUserType', strUserType);
+                    localStorage.setItem('NewPassword', strPass);
 
-				$("#successMessage").removeClass("d-none");
-				$("#successMessage").append("Nuevo usuario registrado con éxito!");
+                    $("#successMessage").removeClass("d-none");
+                    $("#successMessage").append("Nuevo usuario registrado con éxito!");
 
-				alert('Nuevo usuario registrado con éxito');
+                    alert('Nuevo usuario registrado con éxito');
 
-				location.href('./index.html');
-			}
+                    location.href('./index.html');
+                }
 
-			storeData();
+                storeData();
 
-		}
-		else if (validPass != validPass1) {
-			function showError () {
-				$("#errorMessage").removeClass("d-none");
-				$("#errorMessage").append("Lo siento, las contraseñas no coinciden");
-			}
-			showError();
+            }
+            else if (validPass != validPass1) {
+                function showError () {
+                    $("#errorMessage").removeClass("d-none");
+                    $("#errorMessage").append("Lo siento, las contraseñas no coinciden");
+                    alert('Las contraseñas no coinciden, inténtalo de nuevo');
+                }
+                showError();
+            }
+            else {
+                alert('hola');
+            }
+            return true;
 		}
 		else {
-			alert('hola');
+			alert('Todos los campos son obligatorios')
+			return false;
 		}
 
 	});
